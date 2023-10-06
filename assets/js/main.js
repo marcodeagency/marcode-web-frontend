e716e1f7c5b4a43aa99d723bd5e1ab6feff6ae6a
Ümid Ayvazov <102453549+UmiD-AyvazoV@users.noreply.github.com>
2023-10-06 17:24:03 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
+//? Burger Menu
+
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 const lines = document.querySelectorAll(".hamburger div");
+const lang = document.querySelector(".lang");
+const bulb = document.querySelector(".bulb");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("slide");
@@ -8,3 +12,19 @@ hamburger.addEventListener("click", () => {
     item.classList.toggle("toggle");
   });
 });
+
+//! Change Language
+
+lang.addEventListener('click', () => {
+  lang.classList.toggle("active");
+});
+
+//* Digital World Animation
+
+const texts = document.querySelectorAll('.btnImage div');
+texts.forEach( text => text.classList.add('active') );
+
+setInterval( () => {
+  bulb.classList.toggle("active");
+  bulb.classList.contains("active") === true ? bulb.setAttribute('src','./assets/img/digitalWorld/on.png') : bulb.setAttribute('src','./assets/img/digitalWorld/off.png');
+}, 5000);
